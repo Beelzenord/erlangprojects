d6223ebcff7cc051aea9214d201aadad26dd8a17
nordpool <beelzenord@gmail.com>
2019-02-13 17:45:43 +0100

    started supervisor process code

---
+++ src/ss_dock_w_sup.erl
@@ -0,0 +1,39 @@
+%%%-------------------------------------------------------------------
+%%% @author fno
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. Feb 2019 4:06 PM
+%%%-------------------------------------------------------------------
+-module(ss_dock_w_sup).
+-author("fno").
+
+%% API
+-export([start_link/0,start_child/2]).
+-export([init/0]).
+
+start_link()->
+  register(supRef,spawn(ss_dock_w_sup,init,[])),
+  {ok,supRef}.
+
+init()->
+  process_flag(trap_exit, true),
+  loop([]).
+
+start_child(Total,Occupied)->
+  supRef ! {start_child,Total,Occupied,self()},
+  {done}.
+
+init(Args)->
+  io:format("my list ~p",[Args]).
+
+
+loop(Children) ->
+  receive
+    {start_child,Total,Occupied,ClientPid}->
+      Pid = spawn_link(ss_dock_w_sup, init_child, [Total,Occupied]),
+      ClientPid ! {ok, Pid}
+     % loop([{Pid, 1, Mod, Func, Args}|Children]);
+     % io:format("received ~p and ",[Total])
+  end.
